2c72340314f4ce17b53d9e848ca263cc26553bd0
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-02-03 16:29:32 -0800

    Users can submit and save posts

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+
+var app = express();
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+require("./controllers/posts.js")(app);
+// Set db
+require("./data/reddit-db");
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/posts/new", function(rew, res) {
+  res.render("posts-new");
+});
+
+app.listen(3000);
