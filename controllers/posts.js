75177a378b63dbf935cc2f3181b1323b0f0210b6
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-02-05 17:16:38 -0800

    Post tests implemented

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,28 +2,58 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
 
-    // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      console.log(err);
-      console.log(post);
-      return res.redirect(`/`);
-    })
-
-  });
-
-  app.get('/', (req, res) => {
-    Post.find({})
-    .then(posts => {
-      res.render("posts-index", { posts });
-    })
-    .catch(err => {
-      console.log(err.message);
-    })
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+            console.log(err)
+            console.log(post)
+            // REDIRECT TO THE ROOT
+            return res.redirect(`/`);
+        })
     });
+
+    //INDEX
+    app.get('/', (req, res) => {
+
+        Post.find({})
+            .then(posts => {
+                res.render("posts-index", {
+                    posts
+                });
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
+
+    app.get('posts/:id', function (req, res) {
+        // LOOK UP THE POST
+        Post.findById(req.params.id).populate('comments').then((post) => {
+            res.render('posts-show', {
+                post
+            })
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    });
+
+    //SUBREDDIT
+    app.get("/n/:subreddit", function (req, res) {
+        Post.find({
+                subreddit: req.params.subreddit
+            })
+            .then(posts => {
+                res.render("posts-index", {
+                    posts
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
 };
