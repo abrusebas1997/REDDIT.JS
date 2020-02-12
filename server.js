3120b1dd0048c021fb016cd1062b9ecbf8c134aa
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-02-12 13:38:55 -0800

    user can sign in

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express()
 const port = 3000
@@ -13,11 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Add after body parser initialization!
 app.use(expressValidator());
+app.use(cookieParser());
 
 // Set db
+require('dotenv').config();
 require('./data/reddit-db');
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 
 
 // Middleware
@@ -26,9 +30,27 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
+
 app.get('/', (req, res) => res.render('posts-index'))
 
-app.get('/posts/new', (req, res) => res.render('posts-new'))
+app.get('/posts/new', (req, res) => res.render('posts-new'));
+
+// Checks Authentication
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+      req.user = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, {
+        complete: true
+      }) || {};
+      req.user = decodedToken.payload;
+    }
+
+    next();
+  };
+  app.use(checkAuth);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
