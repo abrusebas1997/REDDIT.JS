1765e57a65d2b8b253e6710cd68aa632acdb66ca
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-02-26 20:14:53 -0800

    Authors for comments are visible

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     app.post("/posts/:postId/comments", function (req, res) {
         // INSTANTIATE INSTANCE OF MODEL
         const comment = new Comment(req.body);
-        console.log('///////')
+        comment.author = req.user._id;
 
         // SAVE INSTANCE OF Comment MODEL TO DB
         comment
--- controllers/posts.js
+++ controllers/posts.js
@@ -48,26 +48,26 @@ module.exports = (app) => {
 
 
   app.get("/posts/:id", function (req, res) {
-        var currentUser = req.user;
-        // LOOK UP THE POST
+   var currentUser = req.user;
+   // LOOK UP THE POST
 
-        Post.findById(req.params.id).populate('comments').populate('author')
-            .then(post => {
-                res.render("posts-show", { post, currentUser });
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
-    });
+   Post.findById(req.params.id).populate({path:'comments', populate: {path: 'author'}}).populate('author')
+       .then(post => {
+           res.render("posts-show", { post, currentUser });
+       })
+       .catch(err => {
+           console.log(err.message);
+       });
+});
     // SUBREDDIT
-   app.get("/n/:subreddit", function (req, res) {
-       var currentUser = req.user;
-       Post.find({ subreddit: req.params.subreddit }).populate('author')
-           .then(posts => {
-               res.render("posts-index", { posts, currentUser });
-           })
-           .catch(err => {
-               console.log(err);
-           });
-   });
+    app.get("/n/:subreddit", function (req, res) {
+     var currentUser = req.user;
+     Post.find({ subreddit: req.params.subreddit }).populate('author')
+         .then(posts => {
+             res.render("posts-index", { posts, currentUser });
+         })
+         .catch(err => {
+             console.log(err);
+         });
+ });
 };
